997963680241199bfdceb9380395f686933a1a55
Gabriel Bretschner <software@kanedo.net>
2012-07-18 18:04:33 +0200

    Rewrite jetzt in schneller und besser

--- file_extensions.sh
+++ file_extensions.sh
@@ -1,25 +1,9 @@
-#!/bin/sh
-# Bash script to get filename details | 16-06-2011 | r3dux
-# Usage: fileParts <extension-with-no-dot> i.e. fileParts cpp
-
+#!/bin/bash
 # For each file with the given extension in the current directory...
-echo "Pfad;Datei;Groesse";
-for file in $(find $1 -not -path */dev/*); do
-	#File exists
-	if [ -e "$file" ]; then 
-		#is not a directory
-		if [ ! -d "$file" ]; then 
-				fullPath="$(perl -e 'use Cwd "abs_path";print abs_path(shift)' $file)"
-				fileWithoutPath=$(basename $file)
-				pathWithoutFile="$(echo $fullPath | sed 's/'$fileWithoutPath'//g')"
-				filesize=$(ls -ahl $file | awk '{ print $5 }')
-				
-				echo $pathWithoutFile";"$fileWithoutPath";"$filesize
-		fi
-	else
-		#echo "No files of type $1 found!"
-		exit 0
-
-	fi # End of if file exists condition				
-
-done # End of for each file loop
+echo "Pfad;Datei;Groesse (in Byte)";
+if [[ "x$1" == "x" ]]
+then
+	echo "no path given - exiting" >&2
+	exit 1;
+fi
+find $1 -not -path */dev/* -type f -execdir bash -c 'echo -en "$PWD;$(basename {});$(stat -f %z {})\n"' \;
