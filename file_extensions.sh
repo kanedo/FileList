8a7ba3066948a5bbd508e12d089b16cd36999174
Gabriel Bretschner <software@kanedo.net>
2012-07-18 15:14:25 +0200

    got all important information

--- file_extensions.sh
+++ file_extensions.sh
@@ -3,13 +3,17 @@
 # Usage: fileParts <extension-with-no-dot> i.e. fileParts cpp
 
 # For each file with the given extension in the current directory...
-for file in ./*; do
+for file in $1/*; do
 
 	# If a file exists with a given extension...
 	if [ -e "$file" ]; then
+		fullPath="$(perl -e 'use Cwd "abs_path";print abs_path(shift)' $file)"
 		fileWithoutPath=$(basename $file)
-		extension=${fileWithoutPath##*.}
-		echo $extension
+		echo "Filename only                   is: " $fileWithoutPath
+		pathWithoutFile="$(echo $fullPath | sed 's/'$fileWithoutPath'//g')"
+		echo "Filepath only                   is: " $pathWithoutFile
+		filesize=$(ls -ahl $file | awk '{ print $5 }')
+		echo "Filesize only                   is: " $filesize
 	else
 		echo "No files of type $1 found!"
 		exit 0
