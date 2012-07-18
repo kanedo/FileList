f933bc99643d257a6141280f702506a190f25b03
Gabriel Bretschner <software@kanedo.net>
2012-07-18 15:44:29 +0200

    search only files no directories

--- file_extensions.sh
+++ file_extensions.sh
@@ -3,20 +3,21 @@
 # Usage: fileParts <extension-with-no-dot> i.e. fileParts cpp
 
 # For each file with the given extension in the current directory...
-echo "find $1"
+echo "Pfad;Datei;Groesse";
 for file in $(find $1); do
 
 	# If a file exists with a given extension...
 	if [ -e "$file" ]; then
-		fullPath="$(perl -e 'use Cwd "abs_path";print abs_path(shift)' $file)"
-		fileWithoutPath=$(basename $file)
-		echo "Filename only                   is: " $fileWithoutPath
-		pathWithoutFile="$(echo $fullPath | sed 's/'$fileWithoutPath'//g')"
-		echo "Filepath only                   is: " $pathWithoutFile
-		filesize=$(ls -ahl $file | awk '{ print $5 }')
-		echo "Filesize only                   is: " $filesize
+		if [ ! -d "$file" ]; then
+			fullPath="$(perl -e 'use Cwd "abs_path";print abs_path(shift)' $file)"
+			fileWithoutPath=$(basename $file)
+			pathWithoutFile="$(echo $fullPath | sed 's/'$fileWithoutPath'//g')"
+			filesize=$(ls -ahl $file | awk '{ print $5 }')
+			
+			echo $pathWithoutFile";"$fileWithoutPath";"$filesize
+		fi;
 	else
-		echo "No files of type $1 found!"
+		#echo "No files of type $1 found!"
 		exit 0
 
 	fi # End of if file exists condition				
